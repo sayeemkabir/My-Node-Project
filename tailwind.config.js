d141ffe4c5d53f65040777213ff81658397c767e
sayeemkabir <rezwanksayeem@gmail.com>
2023-03-02 20:32:06 +0600

    test2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,35 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        'mycolor': 'red',
+      }
+
+      
       
     },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    screens: {
+      'sm': '540px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+      '3xl': '1800px',
+    }
   },
   plugins: [],
 }
